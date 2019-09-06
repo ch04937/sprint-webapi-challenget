5e8f4062b7e9d195b0a9e168d76f6931b6373284
ch04937 <97hernandez.c@gmail.com>
2019-09-06 11:17:46 -0700

    MVP

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
+//dependencies
 const express = require('express');
+const helmet = require('helmet');
 
-server = express();
+
+const server = express();
+
 
 //global middleware
 server.use(express.json());
+server.use(helmet());
+
+//route handlers
+const projectRouter = require('./routes/projectRouter.js');
+const actionsRouter = require('./routes/actionsRouter.js');
+
+//local middleware - routers
+server.use('/api/project', projectRouter);
+server.use('/api/actions', actionsRouter);
 
 //test for a working server
 server.get('/', (req, res) => {
     res.send(`its working its working`)
-})
+});
 
 module.exports= server;
